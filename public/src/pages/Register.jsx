ce7fba1499756c5e90eeb965680b9f2ce721bdc5
Surbhi1511-yadav <surbhi.yadav_cs19@gla.ac.in>
2022-04-28 19:02:52 +0530

    commits

--- public/src/pages/Register.jsx
+++ public/src/pages/Register.jsx
@@ -1,5 +1,5 @@
 import React,{useState, useEffect} from 'react';
-import {Link, Navigate, useNavigate} from "react-router-dom";
+import {Link,useNavigate} from "react-router-dom";
 import styled from "styled-components";
 import Logo from "../assets/logo.svg";
 import {ToastContainer,toast} from "react-toastify";
